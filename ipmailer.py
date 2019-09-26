9eebd33e85ac2e2c70710cfc52989e04f0621c66
Dianyi Jiang <dianyij@ur.local>
2019-09-26 14:06:14 +1200

    updating code

--- ipmailer.py
+++ ipmailer.py
@@ -1,65 +1,84 @@
-#! /usr/bin/python
+#!/usr/bin/env python
+
 import subprocess
 import smtplib
 from email.mime.text import MIMEText
 import datetime
 import time
 import platform
+import sys
 
-def get_ip():
-    arg ='ip route list'
-    out = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-    data = out.communicate()
-    split_data = data[0].split()
-    info = split_data[split_data.index('src') + 1]
-    return info
+def get_public_ip():
+    arg ='dig +short myip.opendns.com @resolver1.opendns.com'
+    return run_in_shell(arg)
+
+def get_private_ip():
+    arg ="hostname -I | cut -d ' ' -f1"
+    return run_in_shell(arg)
 
 def get_df():
     arg = 'df -h'
-    out = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-    info = ''
-    for line in out.stdout:
-        info += line
-    return info
+    return run_in_shell(arg)
 
 def get_syslog():
     arg = 'tail /var/log/syslog'
-    out = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-    info = ''
-    for line in out.stdout:
-        info += line
-    return info
+    return run_in_shell(arg)
+
+def run_in_shell(arg):
+    try:
+        out = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
+        return out.communicate()[0].decode("utf-8")
+    except Exception as e:
+        log_error(e)
+        return ""
+
+def log_error(e):
+    arg = 'echo "%s" >> ~/.ipmailer.log' % e
+    run_in_shell(arg)
 
 def mail():
     try:
         # set account
-        to = 'to@gmail.com'
-        gmail_user = 'from@gmail.com'
-        gmail_password = 'gmail app passwd'
+        receiver = 'to@gmail.com'
+        sender = 'from@gmail.com'
+        password = 'password' # or App Specific password (if 2 step verification is on)
+
+        if len(sys.argv) > 3:
+            if sys.argv[1]:
+                receiver = sys.argv[1]
+            if sys.argv[2]:
+                sender = sys.argv[2]
+            if sys.argv[3]:
+                password = sys.argv[3]
+
         smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
-        smtpserver.ehlo()
         smtpserver.starttls()
-        smtpserver.ehlo
-        smtpserver.login(gmail_user, gmail_password)
+        smtpserver.login(sender, password)
         # mail body
-        today = datetime.date.today()
-        mail_body = "version: %s\nuname: %s" %(platform.version(), platform.uname())
-        mail_body += '\n\n' + get_df()
-        mail_body += '\n' + get_syslog()
+        mail_body = "version: %s\nuname: %s" % (platform.version(), platform.uname())
+        mail_body += '\n'
+        mail_body += '\nPublic  IP: %s' % get_public_ip()
+        mail_body += '\nPrivate IP: %s' % get_private_ip()
+        mail_body += '\nDisk Usage: \n%s' % get_df()
+        mail_body += '\n/var/log/syslog: \n%s' % get_syslog()
         # compose email
         msg = MIMEText(mail_body)
-        msg['Subject'] = "RPI @ "+ get_ip() +" started up on %s" % today.strftime('%b %d %Y')
-        msg['From'] = gmail_user
-        msg['To'] = to
+        msg['Subject'] = "Server @ "+ get_public_ip() +" started up"
+        msg['From'] = "Server Info <%s>" % sender
+        msg['To'] = sender
         # send email
-        smtpserver.sendmail(gmail_user, [to], msg.as_string())
+        smtpserver.sendmail(sender, [receiver], msg.as_string())
         smtpserver.quit()
         return False
-    except:
-        # service is not ready on booting
+    except Exception as e:
+        log_error(e)
         return True
 
 cont = True
+counter = 0
 while cont:
     cont = mail()
     time.sleep(1)
+    counter += 1
+    if counter > 60:
+      cout = False
